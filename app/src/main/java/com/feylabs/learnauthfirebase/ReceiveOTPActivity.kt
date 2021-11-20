4265c511bcdae99f98a4db38094ae2a5f196dc3e
henryaugusta <henryaugusta4@gmail.com>
2021-11-20 20:36:59 +0700

    finish UI ReceiveOTP

---
+++ app/src/main/java/com/feylabs/learnauthfirebase/ReceiveOTPActivity.kt
@@ -0,0 +1,11 @@
+package com.feylabs.learnauthfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReceiveOTPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receive_otpactivity)
+    }
+}
